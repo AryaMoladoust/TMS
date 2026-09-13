27a0e994b1589ee7076be2d896b8b6c60176ac75
unknown <abolfazlmoladoust7@gmail.com>
2026-09-13 22:25:46 +0330

    fix auth login page build

---
+++ app/auth/login/page.js
@@ -0,0 +1,14 @@
+export default function LoginPage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <h1>صفحه ورود</h1>
+    </main>
+  );
+}
